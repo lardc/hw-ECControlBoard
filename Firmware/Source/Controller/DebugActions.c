24b8da286fe009275be8b266b30456034948756e
VShurdov <v.shurdov@proton-electrotex.com>
2020-08-26 20:42:29 +0300

    Добавлены отладочные функции

--- Firmware/Source/Controller/DebugActions.c
+++ Firmware/Source/Controller/DebugActions.c
@@ -11,3 +11,54 @@
 
 // Functions
 //
+void DBGACT_BlinkExtLed()
+{
+	LL_SetStateExtLed(TRUE);
+	CONTROL_DelayMs(1000);
+	LL_SetStateExtLed(FALSE);
+}
+//-----------------------------
+
+void DBGACT_GenerateImpulseToExtLineSync1()
+{
+	LL_SetStateExtLineSynce1(TRUE);
+	CONTROL_DelayMs(100);
+	LL_SetStateExtLineSynce1(FALSE);
+}
+//-----------------------------
+
+void DBGACT_GenerateImpulseToExtLineSync2()
+{
+	LL_SetStateExtLineSynce2(TRUE);
+	CONTROL_DelayMs(100);
+	LL_SetStateExtLineSynce2(FALSE);
+}
+//-----------------------------
+
+void DBGACT_GenerateImpulseToIntLineSync1()
+{
+	LL_SetStateIntLineSynce1(TRUE);
+	CONTROL_DelayMs(100);
+	LL_SetStateIntLineSynce1(FALSE);
+}
+//-----------------------------
+
+void DBGACT_GenerateImpulseToIntLineSync2()
+{
+	LL_SetStateIntLineSynce2(TRUE);
+	CONTROL_DelayMs(100);
+	LL_SetStateIntLineSynce2(FALSE);
+}
+//-----------------------------
+
+bool DBGACT_ReadStateIntLineSync1()
+{
+	return LL_GetStateIntLineSynce1();
+}
+//-----------------------------
+
+bool DBGACT_ReadStateIntLineSync2()
+{
+	return LL_GetStateIntLineSynce2();
+}
+//-----------------------------
--- Firmware/Source/Platform/DeviceObjectDictionary.h
+++ Firmware/Source/Platform/DeviceObjectDictionary.h
@@ -7,6 +7,17 @@
 #define ACT_FAULT_CLEAR					3	// Очистка fault
 #define ACT_WARNING_CLEAR				4	// Очистка warning
 
+#define ACT_FAN_ON						48	// Включить вентилятор
+#define ACT_FAN_OFF						49	// Отключить вентилятор
+
+#define ACT_DBG_PULSE_EXT_SYNC1			50	// Одиночный импусль во внешнюю цепь SYNC_1
+#define ACT_DBG_PULSE_EXT_SYNC2			51	// Одиночный импусль во внешнюю цепь SYNC_2
+#define ACT_DBG_PULSE_INT_SYNC1			52	// Одиночный импусль во внутреннюю цепь SYNC_1
+#define ACT_DBG_PULSE_INT_SYNC2			53	// Одиночный импусль во внутреннюю цепь SYNC_2
+#define ACT_DBG_IS_STATE_INT_SYNC_1		54	// Состояние внутренней цепи SYNC_1
+#define ACT_DBG_IS_STATE_INT_SYNC_2		55	// Состояние внутренней цепи SYNC_2
+#define ACT_DBG_PULSE_EXT_LED			56	// Одиночный импусль внешнего светодиода
+
 #define ACT_SAVE_TO_ROM					200	// Сохранение пользовательских данных во FLASH процессора
 #define ACT_RESTORE_FROM_ROM			201	// Восстановление данных из FLASH
 #define ACT_RESET_TO_DEFAULT			202	// Сброс DataTable в состояние по умолчанию
@@ -18,6 +29,7 @@
 // Сохраняемые регистры
 
 // Несохраняемы регистры чтения-записи
+#define REG_DBG_STATE					150	// Регистр режима Отладки
 
 // Регистры только чтение
 #define REG_DEV_STATE					192	// Регистр состояния
@@ -26,6 +38,10 @@
 #define REG_WARNING						195	// Регистр Warning
 #define REG_PROBLEM						196	// Регистр Problem
 #define REG_OP_RESULT					197	// Регистр результата операции
+
+#define REG_DBG_INT_SYNC1				251	// Регистр состояния внутреннейи линии SYNC1
+#define REG_DBG_INT_SYNC2				252	// Регистр состояния внутреннейи линии SYNC2
+#define REG_FAN_STATE					253	// Регистр управления вентилятором
 // -----------------------------
 
 // Operation results
