111ed26c689413b4c1e88858b8a0f043d0c130e2
asytyi <alexander.a.sytyi@gmail.com>
2020-10-05 13:40:25 +0300

    Добавлена инициализация массива настроек slave-узлов

--- Firmware/Source/Controller/Global.h
+++ Firmware/Source/Controller/Global.h
@@ -3,12 +3,14 @@
 
 // Definitions
 // 
-#define	SCCI_TIMEOUT_TICKS					1000	// Таймаут интерфейса SCCI (в мс)
-#define EP_WRITE_COUNT						0		// Количество массивов для записи
-#define EP_COUNT							0		// Количество массивов для чтения
-#define ENABLE_LOCKING						FALSE	// Защита NV регистров паролем
+#define	SCCI_TIMEOUT_TICKS			1000	// Таймаут интерфейса SCCI (в мс)
+#define EP_WRITE_COUNT				0		// Количество массивов для записи
+#define EP_COUNT					0		// Количество массивов для чтения
+#define ENABLE_LOCKING				FALSE	// Защита NV регистров паролем
 
 // Временные параметры
-#define TIME_LED_BLINK						500		// Мигание светодиодом (в мс)
+#define TIME_LED_BLINK				500		// Мигание светодиодом (в мс)
+
+#define MAX_SLAVE_NODES				10		// Максимальное чилсо slave-узлов
 
 #endif //  __GLOBAL_H
--- Firmware/Source/Controller/HighLevel/Common.c
+++ Firmware/Source/Controller/HighLevel/Common.c
@@ -1,27 +1,60 @@
 // Header
 #include "Common.h"
 
+// Includes
+#include "DataTable.h"
+#include "DeviceObjectDictionary.h"
+
 // Types
+//
 typedef enum __NodeName
 {
-	NAME_Multiplexer = 1,
-	NAME_DCCurrent = 2,
-	NAME_DCHighVoltage = 3,
-	NAME_DCVoltage1 = 4,
-	NAME_DCVoltage2 = 5,
-	NAME_DCVoltage3 = 6,
-	NAME_ACVoltage1 = 7,
-	NAME_ACVoltage2 = 8
+	NAME_Multiplexer = 0,
+	NAME_DCCurrent = 1,
+	NAME_DCHighVoltage = 2,
+	NAME_DCVoltage1 = 3,
+	NAME_DCVoltage2 = 4,
+	NAME_DCVoltage3 = 5,
+	NAME_ACVoltage1 = 6,
+	NAME_ACVoltage2 = 7
 
 } NodeName;
 
 typedef struct __SlaveNode
 {
+	bool Initialized;
 	bool Emulation;
 	uint16_t NodeID;
 
 } SlaveNode, *pSlaveNode;
 
-// Functions
+// Variables
+//
+static SlaveNode Nodes[MAX_SLAVE_NODES] = {0};
 
+// Forward functions
+//
+void COMM_InitNode(NodeName Name, uint16_t RegNodeID, bool RegEmulation);
+
+// Functions
+//
+void COMM_InitSlaveArray()
+{
+	COMM_InitNode(NAME_Multiplexer,		REG_SLAVE_NID_MULTIPLEXER,	REG_EMULATE_MULTIPLEXER);
+	COMM_InitNode(NAME_DCCurrent,		REG_SLAVE_NID_DC_CURRENT,	REG_EMULATE_DC_CURRENT);
+	COMM_InitNode(NAME_DCHighVoltage,	REG_SLAVE_NID_DC_HV,		REG_EMULATE_DC_HV);
+	COMM_InitNode(NAME_DCVoltage1,		REG_SLAVE_NID_DC_VOLTAGE1,	REG_EMULATE_DC_VOLTAGE1);
+	COMM_InitNode(NAME_DCVoltage2,		REG_SLAVE_NID_DC_VOLTAGE2,	REG_EMULATE_DC_VOLTAGE2);
+	COMM_InitNode(NAME_DCVoltage3,		REG_SLAVE_NID_DC_VOLTAGE3,	REG_EMULATE_DC_VOLTAGE3);
+	COMM_InitNode(NAME_ACVoltage1,		REG_SLAVE_NID_AC_VOLTAGE1,	REG_EMULATE_AC_VOLTAGE1);
+	COMM_InitNode(NAME_ACVoltage2,		REG_SLAVE_NID_AC_VOLTAGE2,	REG_EMULATE_AC_VOLTAGE2);
+}
+//-----------------------------
+
+void COMM_InitNode(NodeName Name, uint16_t RegNodeID, bool RegEmulation)
+{
+	Nodes[Name].Initialized = true;
+	Nodes[Name].Emulation = DataTable[RegEmulation];
+	Nodes[Name].NodeID = DataTable[RegNodeID];
+}
 //-----------------------------
