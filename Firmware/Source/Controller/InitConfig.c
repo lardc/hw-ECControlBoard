5515ce7cea6bdf29d19ed6d94796cef940ea5683
asytyi <alexander.a.sytyi@gmail.com>
2020-10-06 17:28:09 +0300

    Выполнена настройка и подключение CAN-мастер интерфейса

--- Firmware/Source/Controller/InitConfig.c
+++ Firmware/Source/Controller/InitConfig.c
@@ -41,7 +41,8 @@ void INITCFG_ConfigCAN()
 	RCC_CAN_Clk_EN(CAN_1_ClkEN);
 	NCAN_Init(SYSCLK, CAN_BAUDRATE, false);
 	NCAN_FIFOInterrupt(true);
-	NCAN_FilterInit(0, CAN_SLAVE_FILTER_ID, CAN_MASTER_FILTER_ID);
+	NCAN_FilterInit(0, CAN_SLAVE_FILTER_ID, CAN_SLAVE_NID_MASK);
+	NCAN_FilterInit(1, CAN_MASTER_FILTER_ID, CAN_MASTER_NID_MASK);
 }
 //------------------------------------------------
 
