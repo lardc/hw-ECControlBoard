5c35efa69442bb43e01d1416d8a1a0f9c0a0cadd
asytyi <alexander.a.sytyi@gmail.com>
2020-10-07 20:55:54 +0300

    Реализована функция коммутации и обёртка

--- Firmware/Source/Controller/HighLevel/Multiplexer.c
+++ Firmware/Source/Controller/HighLevel/Multiplexer.c
@@ -8,16 +8,26 @@
 
 // Functions
 //
-bool MUX_ExecuteCommutation(uint16_t NodeID, MeasurementType MeasureType, DL_Case Case, uint16_t Position,
-		DL_InputType InputType)
+bool MUX_Commutate(uint16_t NodeID, MeasurementType MeasureType, DL_Case Case, uint16_t Position,
+		DL_InputType InputType, uint16_t LeakageType, uint16_t Polarity)
 {
 	bool result = false;
 
 	if(BHL_WriteRegister(NodeID, MUX_REG_TYPE_MEASURE, MeasureType))
 		if(BHL_WriteRegister(NodeID, MUX_REG_TYPE_CASE, Case))
 			if(BHL_WriteRegister(NodeID, MUX_REG_POSITION_OF_CASE, Position))
-				if(BHL_WriteRegister(NodeID, MUX_REG_TYPE_SIGNAL_CTRL, InputType));
+				if(BHL_WriteRegister(NodeID, MUX_REG_TYPE_SIGNAL_CTRL, InputType))
+					if(BHL_WriteRegister(NodeID, MUX_REG_TYPE_LEAKAGE, LeakageType))
+						if(BHL_WriteRegister(NodeID, MUX_REG_TYPE_POLARITY, Polarity))
+							result = true;
 
 	return result;
+}
+//-----------------------------
+
+bool MUX_CommutateObject(MuxObject Settings)
+{
+	return MUX_Commutate(Settings.NodeID, Settings.MeasureType, Settings.Case,
+			Settings.Position, Settings.InputType, Settings.LeakageType, Settings.Polarity);
 }
 //-----------------------------
--- Firmware/Source/Controller/HighLevel/Multiplexer.h
+++ Firmware/Source/Controller/HighLevel/Multiplexer.h
@@ -4,8 +4,21 @@
 // Include
 #include "stdinc.h"
 
+// Types
+typedef struct __MuxObject
+{
+	uint16_t NodeID;
+	MeasurementType MeasureType;
+	DL_Case Case;
+	DL_InputType InputType;
+	uint16_t Position;
+	uint16_t LeakageType;
+	uint16_t Polarity;
+
+} MuxObject, *pMuxObject;
+
 // Functions
 //
-
+bool MUX_CommutateObject(MuxObject Settings);
 
 #endif // __MULTIPLEXER_H
