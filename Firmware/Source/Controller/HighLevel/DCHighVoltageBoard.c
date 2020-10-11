154c8d6f0efebcd967e9d29228c374254e225a84
asytyi <alexander.a.sytyi@gmail.com>
2020-10-11 13:20:28 +0300

    Добавлена библиотека DCHighVoltageBoard

---
+++ Firmware/Source/Controller/HighLevel/DCHighVoltageBoard.c
@@ -0,0 +1,93 @@
+// Header
+#include "DCHighVoltageBoard.h"
+
+// Includes
+#include "BCCIMHighLevel.h"
+
+// Functions
+//
+ExecutionResult DCHV_Execute()
+{
+	pSlaveNode NodeData = COMM_GetSlaveDevicePointer(NAME_DCHighVoltage);
+	pDCHVoltageBoardObject Settings = (pDCHVoltageBoardObject)NodeData->Settings;
+
+	if(Settings != NULL)
+	{
+		if(!NodeData->Emulation)
+		{
+			uint16_t VoltageLow = (uint16_t)(Settings->Setpoint.Voltage & 0xFFFF);
+			uint16_t CurrentLow = (uint16_t)(Settings->Setpoint.Current & 0xFFFF);
+			uint16_t NodeID = NodeData->NodeID;
+
+			if(BHL_WriteRegister(NodeID, DCHV_REG_CURRENT_SETPOINT, CurrentLow))
+				if(BHL_WriteRegister(NodeID, DCHV_REG_VOLTAGE_SETPOINT, VoltageLow))
+					if(BHL_Call(NodeID, DCHV_ACT_START_PROCESS))
+						return ER_NoError;
+		}
+		else
+			return ER_NoError;
+
+		return ER_InterfaceError;
+	}
+	else
+		return ER_SettingsError;
+}
+//-----------------------------
+
+ExecutionResult DCHV_ReadResult()
+{
+	pSlaveNode NodeData = COMM_GetSlaveDevicePointer(NAME_DCHighVoltage);
+	pDCHVoltageBoardObject Settings = (pDCHVoltageBoardObject)NodeData->Settings;
+
+	if(Settings != NULL)
+	{
+		if(!NodeData->Emulation)
+		{
+			uint16_t CurrentLow = 0, VoltageLow = 0;
+			uint16_t NodeID = NodeData->NodeID;
+
+			if(BHL_ReadRegister(NodeID, DCHV_REG_CURRENT_RESULT, &CurrentLow))
+				if(BHL_ReadRegister(NodeID, DCHV_REG_VOLTAGE_RESULT, &VoltageLow))
+				{
+					Settings->Result.Current = CurrentLow;
+					Settings->Result.Voltage = VoltageLow;
+					return ER_NoError;
+				}
+		}
+		else
+		{
+			Settings->Result.Current = DCHV_EMULATION_RES_CURRENT;
+			Settings->Result.Voltage = DCHV_EMULATION_RES_VOLTAGE;
+
+			return ER_NoError;
+		}
+
+		return ER_InterfaceError;
+	}
+	else
+		return ER_SettingsError;
+}
+//-----------------------------
+
+ExecutionResult DCHV_Stop()
+{
+	pSlaveNode NodeData = COMM_GetSlaveDevicePointer(NAME_DCHighVoltage);
+	pDCHVoltageBoardObject Settings = (pDCHVoltageBoardObject)NodeData->Settings;
+
+	if(Settings != NULL)
+	{
+		if(!NodeData->Emulation)
+		{
+			uint16_t NodeID = NodeData->NodeID;
+			if(BHL_Call(NodeID, DCHV_ACT_STOP_PROCESS))
+				return ER_NoError;
+		}
+		else
+			return ER_NoError;
+
+		return ER_InterfaceError;
+	}
+	else
+		return ER_SettingsError;
+}
+//-----------------------------
---
+++ Firmware/Source/Controller/HighLevel/DCHighVoltageBoardDictionary.h
@@ -0,0 +1,15 @@
+#ifndef __DC_HIGH_VOLTAGE_BOARD_DICT_H
+#define __DC_HIGH_VOLTAGE_BOARD_DICT_H
+
+// Команды
+#define DCHV_ACT_START_PROCESS				100	// Страрт процесса формирования напряяжения
+#define DCHV_ACT_STOP_PROCESS				101	// Остановка процесса формирования напряяжения
+
+// Регистры
+#define DCHV_REG_CURRENT_SETPOINT			128	// Значение задания тока
+#define DCHV_REG_VOLTAGE_SETPOINT			129	// Значение задания напряжения
+
+#define DCHV_REG_CURRENT_RESULT				200	// Значение измерения тока
+#define DCHV_REG_VOLTAGE_RESULT				201	// Значение измерения напряжения
+
+#endif // __DC_HIGH_VOLTAGE_BOARD_DICT_H
