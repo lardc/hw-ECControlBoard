a9d015176c1c3dbece809f230cbcb5446710abc6
asytyi <alexander.a.sytyi@gmail.com>
2020-11-04 21:18:47 +0300

    Добавлена библиотека калибровки тока утечки

---
+++ Firmware/Source/Controller/HighLevel/LogicCalibrateLeakage.c
@@ -0,0 +1,21 @@
+// Header
+#include "LogicCalibrateLeakage.h"
+
+// Includes
+#include "Logic.h"
+#include "Controller.h"
+#include "Common.h"
+#include "CommonDictionary.h"
+#include "DataTable.h"
+#include "DeviceObjectDictionary.h"
+#include "Global.h"
+#include "Multiplexer.h"
+#include "ACVoltageBoard.h"
+#include "DCHighVoltageBoard.h"
+
+// Functions
+void CLEAK_HandleMeasurement()
+{
+	//
+}
+//-----------------------------
---
+++ Firmware/Source/Controller/HighLevel/LogicCalibrateLeakage.h
@@ -0,0 +1,10 @@
+#ifndef __LOGIC_CALIBRATE_LEAKAGE_H
+#define __LOGIC_CALIBRATE_LEAKAGE_H
+
+// Include
+#include "stdinc.h"
+
+// Functions
+void CLEAK_HandleMeasurement();
+
+#endif // __LOGIC_CALIBRATE_LEAKAGE_H
