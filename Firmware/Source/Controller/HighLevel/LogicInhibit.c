9e996215101a8783feb4c1cb32e631f3e2c082cc
asytyi <alexander.a.sytyi@gmail.com>
2020-11-17 00:09:41 +0300

    Код из напряжения запрета совсем выпилен

--- Firmware/Source/Controller/HighLevel/LogicInhibit.c
+++ Firmware/Source/Controller/HighLevel/LogicInhibit.c
@@ -18,22 +18,13 @@ void INHIBIT_HandleMeasurement()
 {
 	if(CONTROL_State == DS_InProcess)
 	{
-		if(COMM_IsSlaveInFaultOrDisabled())
-			CONTROL_SwitchToFault(ER_WrongState, FAULT_EXT_GR_COMMON);
-
 		switch(CONTROL_SubState)
 		{
 			case DSS_Inhibit_StartTest:
-				if(COMM_AreSlavesInStateX(CDS_Ready))
-					CONTROL_SetDeviceState(DS_InProcess, DSS_Inhibit_ReadResult);
-				else
-					CONTROL_SwitchToFault(ER_WrongState, FAULT_EXT_GR_COMMON);
-				break;
-
 			case DSS_Inhibit_ReadResult:
 				{
 					DataTable[REG_OP_RESULT] = OPRESULT_OK;
-					DataTable[REG_RESULT_INHIBIT_VOLTAGE] = 228;
+					DataTable[REG_RESULT_INHIBIT_VOLTAGE] = 0;
 					CONTROL_SetDeviceState(DS_Ready, DSS_None);
 				}
 				break;
